5759c38957a6352c4a7d74bcf13f91068ee0c1f2
Georgy Khaydenko <ghaidenko@gmail.com>
2016-04-02 14:47:19 +0400

    Change digits to words

--- HelloSwiftMobileWorld2/ViewController.swift
+++ HelloSwiftMobileWorld2/ViewController.swift
@@ -8,18 +8,63 @@
 
 import UIKit
 
+//AutoLayout
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+   
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        let userName = inputField.text ?? "Anonimus"
+        let greeting = userName + " Hello and welcome to the world of mobile apps!"
+        greetingLabel.text = greeting
+        
+    }
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        inputField.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
+
+extension ViewController:UITextFieldDelegate {
+    
+    //когда пользователь нажимает на ввод на устройстве с клавиатуры вызывается этот метод
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // По сути это позволяет скрыть клавиатуру
+        textField.resignFirstResponder()
+        return true
+    }
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        var appliedString = (textField.text as? NSString)?.stringByReplacingCharactersInRange(range, withString: string)
+        
+        let elementsToConvert = ["1":"One",
+                                 "2":"Two",
+                                 "3":"Three",
+                                 "4":"Four",
+                                 "5":"Five",
+                                 "6":"Six",
+                                 "7":"Seven",
+                                 "8":"Eight",
+                                 "9":"Nine"]
+        
+        for (digit,letters) in elementsToConvert{
+            appliedString = appliedString?.stringByReplacingOccurrencesOfString(digit
+                , withString: letters)
+        }
+        textField.text = appliedString
+        
+        return false
+    }
+    
+}
+
+
+
+
+
 
